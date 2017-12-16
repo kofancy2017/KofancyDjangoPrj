48bd1b48107ad03715a3a09a9d573411cd4c5f0f
kofancy2017 <15491914@qq.com>
2017-12-17 07:50:48 +0800

    初始版本

--- polls/models.py
+++ polls/models.py
@@ -7,11 +7,18 @@ from django.utils import timezone
 # Create your models here.
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=500)
-    pub_date = models.DateTimeField("date published")
+    question_text = models.CharField(verbose_name="问题",max_length=500)
+
+    pub_date = models.DateTimeField(verbose_name="发布日期")
+
 
     def __str__(self):
         return  self.question_text
+
+    class Meta:
+        verbose_name = "问题"
+        verbose_name_plural = verbose_name
+
 
     def was_published_recently(self):
         #return self.pub_date >= timezone.now()-datetime.timedelta(days=1)
@@ -24,11 +31,14 @@ class Question(models.Model):
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
-    choice_text = models.CharField(max_length=1000)
-    votes = models.IntegerField(default=0)
+    choice_text = models.CharField(verbose_name="选项",max_length=1000)
+    votes = models.IntegerField(verbose_name="投票数",default=0)
 
     def __str__(self):
         return self.choice_text
 
+    class Meta:
+        verbose_name = "选项"
+        verbose_name_plural = verbose_name
 
 
